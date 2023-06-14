6317279fc32b2a21cc694bacc4390938d764e6b1
Andres Duran <andresduranrod@gmail.com>
2023-06-14 01:15:53 -0600

    adjusting conversation to use systems instrucctions instead of use User profile

--- virtualHomeAssistant/controllers/chat_controller.py
+++ virtualHomeAssistant/controllers/chat_controller.py
@@ -2,18 +2,12 @@ from datetime import datetime
 
 class WelcomeChat():
     intro = "Hello Assistant, The following people just arrived home, please give us a personalized greeting, analyze the time of my systems and response with a personalized greeting, respectful but friendly, as Jarvis from Iron Man would:"
-    information_decription = """After that give an update about the system information following the next steps:
-(1) Go through the information one by one and consider whether the information is potentially relevant to inform or not.
-(2) If you detect a value or some set of values together could be an issue, needs action on my part, give me a recommendation about it.
-(3) Let me know only the important information.
-(4) Remember that you must always answer me in Spanish."""
 
     @classmethod
     def format_welcome_text(cls,people_arriving,devices_information):       
         people_meeting_text = cls.get_arriving_format(people_arriving)
         text_device_information = cls.text_from_list(devices_information)
-        information_text = f"{cls.information_decription}\n{text_device_information}"
-        final_text = f"{cls.intro}{people_meeting_text}\n{information_text}"
+        final_text = f"{cls.intro}{people_meeting_text}\n{text_device_information}"
         return final_text
     
     @classmethod
@@ -55,7 +49,7 @@ class WelcomeGuestChat():
 
     
 class GoodMorningChat():
-    meeting_description = "We are waking up, please give us a personalized greeting as Jarvis from Iron Man would, respectful but friendly, without repeating the information I give you. Remember that you must always answer me in Spanish"
+    meeting_description = "We are waking up, please give us a personalized greeting and tell us the time as Jarvis from Iron Man would, respectful but friendly, without repeating the information I give you. Remember that you must always answer me in Spanish."
     @classmethod
     def format_good_morning_text(cls,devices_information):
         text_device_information = cls.text_from_list(devices_information)
