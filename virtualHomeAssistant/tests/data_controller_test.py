a6d4fc6b49669c91b735b89bfc7101a2c0fb77aa
Andres Duran <andresduranrod@gmail.com>
2024-07-23 21:13:40 -0600

    organize test files

--- virtualHomeAssistant/tests/data_controller_test.py
+++ virtualHomeAssistant/tests/data_controller_test.py
@@ -16,38 +16,44 @@ def main():
     data_config = ConfigurationReader.read_config_file()
     data_controller = DataController(data_config)
     test_people_information(data_controller)
-    #test_calendars_events(data_controller)
-    #test_sensors_information(data_controller)
-    #test_general_devices_information(data_controller)
-    #test_people_arriving_home(data_controller)
-    #test_important_devices(data_controller)
+    test_calendars_events(data_controller)
+    test_sensors_information(data_controller)
+    test_general_devices_information(data_controller)
+    test_people_arriving_home(data_controller)
+    test_important_devices(data_controller)
 
 def test_people_information(data_controller:DataController):
+    print("\n*[TEST] test_people_information")
     people_information: list[Person] = data_controller.get_people_information()
     for people in people_information:
         print(people.get_information())
 
 def test_calendars_events(data_controller:DataController):
+    print("\n*[TEST] test_calendars_events")
     calendars_events: list[Calendar] = data_controller.get_calendar_events()
     for calendar in calendars_events:
         print(calendar.to_text())
 
 def test_sensors_information(data_controller:DataController):
+    print("\n*[TEST] test_sensors_information")
     sensor_information: list[Sensor] = data_controller.get_sensors_information()
     for sensor in sensor_information:
         print(sensor.to_text())
 
 def test_general_devices_information(data_controller:DataController):
+    print("\n*[TEST] test_general_devices_information")
     general_devices_information = data_controller.get_general_devices_information()
     for general_devices in general_devices_information:
         print(general_devices.to_text())
 
 def test_people_arriving_home(data_controller:DataController):
+    print("\n*[TEST] test_people_arriving_home")
     people_arriving_home: list[Person] = data_controller.get_people_arriving_home()
     for person in people_arriving_home:
         print(person.get_information())
 
 def test_important_devices(data_controller:DataController):
+    print("\n*[TEST] test_important_devices")
     important_devices = data_controller.get_important_devices()
     for device in important_devices:
         print(device.to_text())
